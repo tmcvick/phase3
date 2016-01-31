e51a26c4b7929988ace0da29c1f34280804a48fa
tmcvick <tmcvick@g.clemson.edu>
2016-01-31 16:01:04 -0500

    Delete Slider

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -14,11 +14,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
-import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -33,7 +30,6 @@ public class Window extends JFrame {
 	private JList<MediaItem> itemList;
 	private SOAPClient soap;
 	private JScrollPane scrollPane;
-	private JSlider timeSlider;
 	private URL serverURL = null;
 
 	public URL getURL() {
@@ -44,8 +40,7 @@ public class Window extends JFrame {
 	private JTextField songUrl;
 	private URL playUrl;
 
-	private int MAX_TIME = 0, MIN_TIME = 0;
-
+	
 	public void setItems(ArrayList<MediaItem> items) {
 		if (mediaItems != null) {
 			mediaItems.clear();
@@ -95,73 +90,67 @@ public class Window extends JFrame {
 
 		JButton btnStop = new JButton("Stop");
 
-		timeSlider = new JSlider(MIN_TIME, MAX_TIME);
-		timeSlider.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent arg0) {
-				if (!timeSlider.getValueIsAdjusting())
-					slider_change();
-			}
-		});
-		timeSlider.setValue(0);
-		timeSlider.setSnapToTicks(true);
-		timeSlider.setPaintTicks(true);
-		timeSlider.setPaintLabels(true);
-		timeSlider.setMajorTickSpacing(10);
-		timeSlider.setMinorTickSpacing(5);
-
 		scrollPane = new JScrollPane();
 
 		songUrl = new JTextField();
 		songUrl.setColumns(10);
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblServerUrl, GroupLayout.PREFERRED_SIZE, 74,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED).addComponent(inputUrl,
-												GroupLayout.PREFERRED_SIZE, 331, GroupLayout.PREFERRED_SIZE))
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblServerUrl, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(inputUrl, GroupLayout.PREFERRED_SIZE, 331, GroupLayout.PREFERRED_SIZE))
 						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 293, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup().addGap(43)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addGroup(groupLayout.createSequentialGroup().addComponent(btnPlay).addGap(18)
-												.addComponent(btnPause).addPreferredGap(ComponentPlacement.UNRELATED)
-												.addComponent(btnStop))
-								.addGroup(groupLayout.createSequentialGroup().addGap(100).addComponent(lblObjectId)
-										.addPreferredGap(ComponentPlacement.RELATED)
-										.addComponent(objectNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-												GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(btnRetrieve))))
-						.addGroup(groupLayout.createSequentialGroup().addGap(17)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(songUrl, GroupLayout.PREFERRED_SIZE, 349,
-												GroupLayout.PREFERRED_SIZE)
-										.addComponent(timeSlider, GroupLayout.PREFERRED_SIZE, 256,
-												GroupLayout.PREFERRED_SIZE))))
-						.addGap(49)));
-		groupLayout
-				.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup().addContainerGap()
-								.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-										.addComponent(lblServerUrl, GroupLayout.PREFERRED_SIZE, 24,
-												GroupLayout.PREFERRED_SIZE)
-								.addComponent(inputUrl, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblObjectId, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-								.addComponent(objectNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-										GroupLayout.PREFERRED_SIZE).addComponent(btnRetrieve))
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-										.createSequentialGroup().addGap(46)
-										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-												.addComponent(btnPlay).addComponent(btnPause).addComponent(btnStop))
-										.addGap(18)
-										.addComponent(timeSlider, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-												GroupLayout.PREFERRED_SIZE)
-										.addGap(18).addComponent(songUrl, GroupLayout.PREFERRED_SIZE,
-												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup().addGap(33).addComponent(scrollPane,
-								GroupLayout.DEFAULT_SIZE, 245, Short.MAX_VALUE))).addContainerGap()));
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(43)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(btnPlay)
+									.addGap(18)
+									.addComponent(btnPause)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnStop))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(100)
+									.addComponent(lblObjectId)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(objectNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnRetrieve))))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(6)
+							.addComponent(songUrl, GroupLayout.PREFERRED_SIZE, 349, GroupLayout.PREFERRED_SIZE)))
+					.addGap(49))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblServerUrl, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+						.addComponent(inputUrl, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblObjectId, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
+						.addComponent(objectNum, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnRetrieve))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(46)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnPlay)
+								.addComponent(btnPause)
+								.addComponent(btnStop))
+							.addGap(18)
+							.addComponent(songUrl, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(33)
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 245, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
 		listmodel = new DefaultListModel<MediaItem>();
 		itemList = new JList<MediaItem>(listmodel);
 		itemList.addListSelectionListener(new ListSelectionListener() {
@@ -185,10 +174,10 @@ public class Window extends JFrame {
 		try {
 			mediaItems = soap.request(Integer.parseInt(objectNum.getText()));
 		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 
@@ -209,9 +198,5 @@ public class Window extends JFrame {
 		objectNum.setText(Integer.toString(ID));
 	}
 
-	public void slider_change() {
-		int newTime = timeSlider.getValue();
-
-	}
 }
 
