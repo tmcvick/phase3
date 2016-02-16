2ada3854b6d8f75d4cd6fa49ab1ddec447e9f1be
Clay Moeller <cmoelle@clemson.edu>
2016-02-16 14:54:23 -0500

    Added FavPanel & LibPanel Classes

---
+++ src/main/java/team8/FavoritesPanel.java
@@ -0,0 +1,7 @@
+package team8;
+
+import javax.swing.JPanel;
+
+public class FavoritesPanel extends JPanel {
+
+}
---
+++ src/main/java/team8/LibraryPanel.java
@@ -0,0 +1,7 @@
+package team8;
+
+import javax.swing.JPanel;
+
+public class LibraryPanel extends JPanel {
+
+}
