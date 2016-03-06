d754627987b10e46bb3de5e5ce84fa482ea76f3a
Dylan Pyle <pyledev@gmail.com>
2016-03-05 19:22:24 -0500

    Cant download media to play

--- src/main/java/team8/Main.java
+++ src/main/java/team8/Main.java
@@ -8,6 +8,25 @@ import gui.LoginFrame;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		/* My Attempt at downloading a file. Doesn't work but can't figure out why, giving up.
+		try {
+		URLConnection conn = new URL("http://127.0.0.1:5001/get/122/Track+1.mp3").openConnection();
+	    InputStream is = conn.getInputStream();
+	    FileOutputStream outstream = new FileOutputStream(new File("Alegro.mp3"));
+	    byte[] buffer = new byte[4096];
+	    int len;
+	    while ((len = is.read(buffer)) > -1) {
+	    	System.out.println("LOOPING");
+	        outstream.write(buffer, 0, len);
+	    }
+	    is.close();
+	    outstream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} */
+		
+		
 		//Set look and feel
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
